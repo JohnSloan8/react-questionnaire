17415c2311620f1643fcbb83f71de938bbfd2f90
johnsloan1984 <johnsloan1984@gmail.com>
2022-01-10 16:47:16 +0000

    done with Router tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,20 @@
 import './App.css';
-import { useState, useEffect } from "react";
-import Axios from 'axios';
+import { Outlet, Link } from "react-router-dom";
 
 function App() {
-  const [listOfUsers, setListOfUsers]= useState([])
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  
-  useEffect(() => {
-    Axios.get("http://localhost:3001/getUsers").then((response) => {
-      setListOfUsers(response.data);
-    })
-  }, [])
-
-  const createUser = () => {
-    Axios.post("http://localhost:3001/createUser", {name, email})
-      .then((response) => {
-        setListOfUsers([...listOfUsers, {name, email}])
-      })
-  }
-
   return (
-    <div className="App">
-      <div className="usersDisplay">
-        {listOfUsers.map((u) => {
-          return (
-          <div id={u.name + "_ID"}>
-            <h1 id={u.name + "_inner_ID"}>Name: {u.name}</h1>
-            <h1 id={u.email + "_ID"}>Email: {u.email}</h1>
-          </div>
-          )})}
-      </div>
-      <div>
-        <input type="text" placeholder="Name..." onChange={(event) => {setName(event.target.value)}} />
-        <input type="text" placeholder="Email..." onChange={(event) => {setEmail(event.target.value)}} />
-        <button onClick={createUser}>Create User</button>
-      </div>
+    <div>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
